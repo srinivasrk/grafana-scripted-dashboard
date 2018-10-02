035d19e7a51ba2ce911026491de7c6bfbd5cc793
srinivasrk <srinivasrk92@gmail.com>
2018-10-02 16:22:04 -0400

    use express instead of polka and remove other routes

--- portal/src/server.js
+++ portal/src/server.js
@@ -1,12 +1,14 @@
 import sirv from 'sirv';
-import polka from 'polka';
+import express from 'express';
 import compression from 'compression';
 import * as sapper from '../__sapper__/server.js';
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
-polka() // You can also use Express
+const app = express();
+
+app // You can also use Express
 	.use(
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
